c55f4d5261a417b44eb9043e6950d0e30a1d5f65
Bhalli Bhai <94342592+BhalliBhai@users.noreply.github.com>
2022-06-26 21:25:16 +0500

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     return(
         <>
 <div class="copyright text-center">
-        <p>Ⓒ <span id="year">{year}</span> All Rights Reserved | Developed by <span class="my-Name"><a target="_blank" rel="noreferrer" href="mailto:bhallibhai69@gmail.com"> Bhalli</a></span> </p>
+        <p>Ⓒ <span id="year">{year}</span> All Rights Reserved | Developed by <span class="my-Name"><a target="_blank" rel="noreferrer" href="mailto:bhalli0006@gmail.com"> Bhalli</a></span> </p>
     </div>        </>
     );
 }
